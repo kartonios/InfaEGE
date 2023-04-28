6521c619570b52932704d739fb82b5258d56f513
kartonios <akhvatov2005@gmail.com>
2023-04-28 12:43:17 +0300

    update

--- n19.py
+++ n19.py
@@ -321,3 +321,20 @@ for s in range(1, 24):
 20 - 20 29 +
 21 - 9 + 
 '''
+
+
+def f(s, c, m):
+    if s >= 351:
+        return c % 2 == m % 2
+    if c == m:
+        return 0
+
+    h = [f(s+1, c+1, m), f(s+4, c+1, m), f(s*2, c+1, m)]
+
+    return any(h) if (c + 1) % 2 == m % 2 else all(h)
+
+print(f'19 {[s for s in range(1, 351) if f(s, 0 , 2)]}')
+
+print(f'20 {[s for s in range(1, 351) if f(s, 0, 3) and not(f(s, 0, 1))]}')
+
+print(f'21 {[s for s in range(1, 351) if (f(s, 0, 2) or f(s, 0, 4)) and not(f(s, 0, 2))]}')
